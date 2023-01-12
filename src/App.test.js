728e36a6f5fd730ddacd5dca7f952c36726a757f
Manuel Venegas <manu.vem9708@gmail.com>
2023-01-12 17:38:26 -0600

    test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from '@testing-library/react';
+import Booking from './components/Booking';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Testing some elements from the booking form, check if people < 0 submit is disabled', () => {
+  const handleSubmit = jest.fn();
+  render(<Booking onSubmit={handleSubmit} />);
+  
+  const rangeInput = screen.getByTestId("people");
+  fireEvent.change(rangeInput, {target: {value : "0"}});
+  
+  const submitButton = screen.getByRole("button");
+  fireEvent.click(submitButton);
+
+  expect(handleSubmit).not.toHaveBeenCalled();
+  expect(submitButton).toHaveAttribute("disabled");
 });
+
